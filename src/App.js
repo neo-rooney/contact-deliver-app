28a5e68aeacc4f7f7fa3081c262cd4d3b7139916
rooney <bch3454@gmail.com>
2023-07-05 13:18:31 +0900

    Feat: Deep Link 테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import QRCode from "react-qr-code";
 
 function App() {
   return (
     <div className="Container">
-      <a href="intent://contacts#Intent;scheme=contacts;package=com.android.contacts;end">
-        연락처 앱 열기
-      </a>
+      <QRCode
+        size={256}
+        style={{ height: "300px", maxWidth: "100%", width: "100%" }}
+        value={"https://contact-deliver-n1g6ka4ry-rooney-dev.vercel.app/"}
+        viewBox={`0 0 256 256`}
+      />
+      <a href="content://com.android.contacts/contacts">연락처 앱 열기</a>
     </div>
   );
 }
