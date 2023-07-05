4bf676ab8a8e7bcfa399f4d204da3f2a4ba5da24
rooney <bch3454@gmail.com>
2023-07-05 13:50:09 +0900

    Feat: Deep Link 테스트

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
         value={"contact-deliver-2g9kdga8o-rooney-dev.vercel.app"}
         viewBox={`0 0 256 256`}
       />
-      <a href="tel:010-1234-5678?name=홍길동">전화하기</a>
+      <a href="content://com.android.contacts/contacts#Intent;action=android.intent.action.INSERT;S.phone=01077777777;end">
+        전화번호 있는거
+      </a>
+      <a href="content://com.android.contacts/contacts">그냥 연락처</a>
     </div>
   );
 }
